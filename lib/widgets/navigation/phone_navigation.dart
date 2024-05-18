06fc9ca72ead1d4f9936b6105105858af3b5b30f
kshuiroy <kshuiroy@hotmail.com>
2024-05-18 22:39:37 +0800

    [General] Adjusted the height of the navigation bar and the width when it is on tablet in portrait orientation.

--- lib/widgets/navigation/phone_navigation.dart
+++ lib/widgets/navigation/phone_navigation.dart
@@ -1,9 +1,11 @@
 import 'dart:ui';
 
+import 'package:ae_live/config/constants.dart';
 import 'package:ae_live/config/tab_items.dart';
 import 'package:ae_live/i18n/translations.g.dart';
 import 'package:ae_live/models/destination_item.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 class PhoneNavigation extends StatelessWidget {
   const PhoneNavigation({
@@ -24,6 +26,30 @@ class PhoneNavigation extends StatelessWidget {
 
   @override
   Widget build(final BuildContext context) {
+    if (ResponsiveBreakpoints.of(context)
+        .largerOrEqualTo(Constants.screenSizeKeyMedium)) {
+      return Row(
+        children: [
+          Expanded(
+            child: _buildNavigationBar(context, true),
+          ),
+          const SizedBox(
+            width: 1.0,
+          ),
+          const Expanded(
+            child: SizedBox(),
+          ),
+        ],
+      );
+    }
+
+    return _buildNavigationBar(context, false);
+  }
+
+  Widget _buildNavigationBar(
+    final BuildContext context,
+    final bool isMediumScreen,
+  ) {
     final Translations t = Translations.of(context);
     final ColorScheme colorScheme = Theme.of(context).colorScheme;
 
@@ -39,6 +65,7 @@ class PhoneNavigation extends StatelessWidget {
           selectedIndex: selectedIndex,
           onDestinationSelected: onDestinationSelected,
           indicatorColor: Colors.transparent,
+          height: isMediumScreen ? 64.0 : 56.0,
           destinations: tabItems
               .map(
                 (final DestinationItem item) => NavigationDestination(
