c65685f60f46795accaafe1323e339f49a8a2b09
kshuiroy <kshuiroy@hotmail.com>
2024-05-27 14:47:12 +0800

    [Home] Fixed the waiting time detail pane will not dismiss after the data is refreshed on large-screen devices.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -69,6 +69,7 @@ class _HomeScreenState extends State<HomeScreen> {
             onFinished: () {
               setState(() {
                 _isLoading = false;
+                _selectedWaitTime = null;
               });
             },
           ),
